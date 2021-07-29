a4e8df189089a8823b5309e087511a7a0025f5df
Cherniavskyi Oleh <ochernyavskyi@gmail.com>
2021-07-29 23:16:22 +0300

    first commit

--- dep-terraform/ecs/tg.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_lb_target_group" "target_group" {
-  name     = "MyTargetGroup2"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
--- dep-terraform/ecs/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-  description = "Default region name"
-  default = "eu-central-1"
-}
-
-variable "public_subnets" {
-  default = ["172.16.0.0/24", "172.16.1.0/24"]
-}
